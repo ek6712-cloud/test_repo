122bd72641b63d76c83f0c49c67dd1ed4e3398f4
DD\ek671 <ek6712@gmail.com>
2025-11-25 17:28:12 +0900

    첫 번째 커밋 README파일 수정 test.java파일 신규 생성

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# test_repo
+# test\_repo
+
 test repository
+
+
+
+date : 2025-11-25
+
---
+++ test.java
@@ -0,0 +1,5 @@
+public class test {
+	public static void main (String[] arge){
+		System.out.println("hello");
+	}
+}
